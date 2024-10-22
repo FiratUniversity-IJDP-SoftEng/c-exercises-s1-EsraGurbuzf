70cce92eaa311e7b2408a3ab0b9221dd1e5844ce
EsraGurbuzf <230543001@firat.edu.tr>
2024-10-22 20:22:34 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+
+int main(void) {
+    int guess;
+    
+    printf("Guess a number between 1 and 5\n");
+    scanf("%d", &guess);
+    
+    if (guess == 3) {
+        printf("You win!\n");
+    } else { 
+        printf("Guess again!\n");
+    }
+}
