118999b0ed064bf52aa0e6f7f8761d33293bd00f
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2024-10-22 21:21:38 +0000

    Notes from AssocProfDrFU

--- ex1.c
+++ ex1.c
@@ -13,3 +13,6 @@ int main(void) {
         printf("Guess again!\n");
     }
 }
+// You say "guess again" but your program stops working, please think that what if the user give the input out of the 1-5 interval, your program stops.
+// But you should continiue to ask user for the right input in this case betweer 1 and 5 inclusive. 
+// User just can use 1-2-3-4-5 numbers, otherwise, you should keep asking again and again. Remind- do while Esra...Thanks.
