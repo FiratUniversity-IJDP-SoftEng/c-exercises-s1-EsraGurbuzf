81df5d3f8a7ef5852798bef51a109a8d49500eaf
EsraGurbuzf <230543001@firat.edu.tr>
2024-10-25 14:51:23 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(void) {
+  char animal[3];
+  printf("Do you have a cat or a dog?\n");
+  scanf("%3s", &animal);
+  if (strcmp(animal, "cat") == 0) {
+    printf("meow");
+  } else if (strcmp(animal, "dog") == 0) {
+    printf("woof");
+  }
+}
