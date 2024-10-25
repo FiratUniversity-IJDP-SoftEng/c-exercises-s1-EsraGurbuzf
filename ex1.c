4dec20e4c8668d7ecb8cb4b64ce0d45880c5da07
EsraGurbuzf <230543001@firat.edu.tr>
2024-10-25 12:15:52 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,18 +1,18 @@
-
 #include <stdio.h>
 
-int main(void) {
-    int guess;
-    
-    printf("Guess a number between 1 and 5\n");
+int main(void){
+  int guess;
+  
+  do {
+    printf("Guess a number between 1 and 5:\n");
     scanf("%d", &guess);
+  } while(guess < 1 || guess > 5);
     
-    if (guess == 3) {
-        printf("You win!\n");
-    } else { 
-        printf("Guess again!\n");
-    }
+
+  if (guess == 3) {
+    printf("You guessed correctly!\n");
+  } else {
+      printf("You guessed incorrectly!\n");
+    (guess != 3);
+  }
 }
-// You say "guess again" but your program stops working, please think that what if the user give the input out of the 1-5 interval, your program stops.
-// But you should continiue to ask user for the right input in this case betweer 1 and 5 inclusive. 
-// User just can use 1-2-3-4-5 numbers, otherwise, you should keep asking again and again. Remind- do while Esra...Thanks.
